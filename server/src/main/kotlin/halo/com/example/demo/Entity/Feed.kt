8b09c6be49c151ec6119928ee487b084477959bf
takubo11 <takubo@talknote.com>
2018-10-28 17:59:51 +0900

    create feed Entity

--- server/src/main/kotlin/halo/com/example/demo/Entity/Feed.kt
+++ server/src/main/kotlin/halo/com/example/demo/Entity/Feed.kt
@@ -1,22 +1,11 @@
 package com.halo.model
 import lombok.Data;
 
-//@Entity
-//@Data
+@Entity
+@Data
 data class Feed (
     val id: Long,
     val message: String,
     val username: String,
     val registDate: String
-) {
-    //とり会えず、真似してみる。
-//    companion object {
-//        private val locale = ULocale("ja_JP")
-//        private val rule = RuleBasedNumberFormat(locale, SPELLOUT)
-//    }
-
-//    val message: String by lazy {
-//        rule.format(value)
-//    }
-
-}
+) {}
